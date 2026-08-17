aa2b91a1f34d9b84958d35a6cb656c5eb9ffb67a
Chandan <developer.chandan.in@gmail.com>
2026-08-17 10:33:49 +0530

    Add Javascript app

---
+++ Javascript/app.js
@@ -0,0 +1,41 @@
+console.log("Hello Chandan");
+console.log("Namaste India");
+console.log("Hello World");
+
+let y = 10;
+let x = 20;
+console.log("sum is : ", y + x);
+
+//this is comment
+
+let pencilPrice = 10;
+let penPrice = 10;
+console.log("The otal price is : ", pencilPrice + penPrice, "Rupees.");
+
+//Arthmatic Operators
+let c = 20;
+let b = 5;
+console.log("c + b =", c + b);
+console.log("c - b =", c - b);
+console.log("c * b =", c * b);
+console.log("c / b =", c / b);
+console.log("c ** b =", c ** b);
+console.log("c % b =", c % b);
+
+//Unary Operators
+let d = 20;
+let e = 10;
+console.log(d++);
+console.log(++d);
+
+//Assignment Operators
+let u = 10;
+let v = 50;
+u = v
+console.log(u);
+
+//Comparision Operators
+
+
+
+
