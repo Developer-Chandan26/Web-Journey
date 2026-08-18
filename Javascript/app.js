fe7b490462e135eea04450407f9ddd295b799138
Chandan <developer.chandan.in@gmail.com>
2026-08-18 16:56:46 +0530

    Update app.js

--- Javascript/app.js
+++ Javascript/app.js
@@ -21,7 +21,7 @@ console.log("c * b =", c * b);
 console.log("c / b =", c / b);
 console.log("c ** b =", c ** b);
 console.log("c % b =", c % b);
-
+ 
 //Unary Operators
 let d = 20;
 let e = 10;
@@ -35,6 +35,36 @@ u = v
 console.log(u);
 
 //Comparision Operators
+let age = 20
+console.log(age <= 18); 
+
+let a = 28
+console.log(a >25);
+
+let mohanAge = 14
+console.log(mohanAge >= 18); //false
+
+let g = 10
+console.log(a != 18);
+
+//values is compare not type
+let n = 5;
+let str = '5'; //value are compared, not type
+console.log(n == str);
+console.log(n === str); // === both are compared type and value
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(0 == '');
+console.log(0 === '');
+console.log(123 == '123');
+console.log(123 === '123');
+
+//comparision for non-number
+console.log('a' < 'A');
+console.log('A' < 'a');
+
+
+
 
 
 
