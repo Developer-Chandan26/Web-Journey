361c169e33768423845207f205ed6465013137a7
Chandan <developer.chandan.in@gmail.com>
2026-08-18 18:59:05 +0530

    Add javascript Practice1

---
+++ Javascript/Practice1.js
@@ -0,0 +1,39 @@
+//else if Statement
+//Traffic light system
+
+let color = "green";
+
+if (color === "red") {
+  console.log("stop! light is red");
+} else if (color === "yellow") {
+  console.log("slow down light is yellow");
+} else if (color === "green") {
+  console.log("go light is green");
+}
+
+//Example 2
+let month = "January";
+
+if (month === "January") {
+  console.log("winter is here");
+} else if (month === "April") {
+  console.log("summer is here");
+}
+
+//Example 3
+let marks = "32";
+
+if (marks >= "80") {
+  console.log("A+");
+} 
+else if (marks >= "60") {
+  console.log(A);
+} 
+else if (marks >= "45") {
+  console.log("B");
+} 
+else if ((marks <= 33)) {
+  console.log("Fail");
+} 
+
+
