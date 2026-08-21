535e65382a8f69c4dcffb2b36f501f2fe660f6f0
Developer_Chandan <developer.chandan.in@gmail.com>
2026-08-21 11:32:40 +0530

    Add Alert&Prompt files

---
+++ Javascript/Alert&Prompt.js
@@ -0,0 +1,9 @@
+//Alert
+//console.log("this is a simple log");
+///console.error("this is an error");
+//console.warn("this is a warning");
+
+//Prompt
+//prompt("Enter your prompt:");
+let name = prompt("Enter your name:");
+console.log(name);
