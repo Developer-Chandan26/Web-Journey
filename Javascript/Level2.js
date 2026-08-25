0446d197b2593402a50aa9c0b44df362bf6602ed
Developer_Chandan <developer.chandan.in@gmail.com>
2026-08-25 16:45:56 +0530

    Javascript level2 files

---
+++ Javascript/Level2.js
@@ -0,0 +1,31 @@
+//Trim Method
+//let msg = "     he   llo       ";
+
+//let password = prompt("set your password");
+//console.log(password.trim());
+
+//let str = "I Love Cod      ing                 ";
+
+//console.log(str.trim());
+
+//upper and lower case method
+let name = "Chandan Prajapati";
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+
+//string method with argument
+//1. indexof
+let msg = "I Love Coding";
+console.log(msg.indexOf("Love"));
+
+//CHAINING METHOD
+let nick = "Chandan          ";
+let newNick = nick.trim().toUpperCase();
+console.log(newNick);
+
+//Slice Method
+let str ="Chandan";
+//console.log(str.slice(0,5));
+console.log(str.slice(4)); //str.lengh
+
+//Replace Method
