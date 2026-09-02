44da8426c072d30807eec9b5ca4b62d1006b09c5
Developer_Chandan <developer.chandan.in@gmail.com>
2026-09-02 17:54:36 +0530

    Add Javascript files

---
+++ Javascript/for_loops.js
@@ -0,0 +1,29 @@
+//long form for print value
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+
+//short form for print for value using for loops
+for(let i=1; i<=10; i++) {
+    console.log(i);
+}
+
+for(let j=20; j>=1; j--){
+    console.log(j);
+}
+
+for(let a=3; a<=30; a = a+3){
+    console.log(a);
+}
+
+//print all odd number (1 to 15)
+for(let num = 1; num <=15; num=num+2){
+    console.log(num);
+}
+
+//print all even numbers (2 to 20)
+for(let k=2; k<=20; k=k+2){
+    console.log(k);
+}
