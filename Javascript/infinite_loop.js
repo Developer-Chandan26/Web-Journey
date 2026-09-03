951b47af3682c2e6fa7ddeb8c524b6d29e399da8
Developer_Chandan <developer.chandan.in@gmail.com>
2026-09-03 17:14:52 +0530

    Add Javascript files

---
+++ Javascript/infinite_loop.js
@@ -0,0 +1,36 @@
+//for(let i=1; i>=0; i++){
+// console.log(i);}
+
+//for (let j=1; j<=5; j--){
+//  console.log(i);}
+
+//for(let k=1; ; k++){
+// console.log(k);}
+
+//Print the multiplication table for 5
+//let n = prompt("write your table  number");
+//n = parseInt(n);
+//for(let i=n; i<=n*10; i=i+n){
+// console.log(i);
+//}
+
+//Nested for loops
+for (let i = 1; i <= 3; i++) {
+  for (let j = 1; j <= 3; j++) {
+    console.log("*");
+  }
+}
+
+//While loop
+let i=1;
+while(i<=5){
+    console.log(i);
+    i++;
+}
+
+let n =prompt("write your number");
+while(n<=60){
+    console.log(n);
+    n++;
+}
+
