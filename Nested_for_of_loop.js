58fb8839ba305b75cc5d6e0baac79e51a0c4127d
Developer_Chandan <developer.chandan.in@gmail.com>
2026-09-15 15:10:20 +0530

    Add loop with array and nested files

---
+++ Nested_for_of_loop.js
@@ -0,0 +1,6 @@
+let heroes = [["Superman", "Spiderman", "wonder Women"], ["thor", "groot","ironman","batman"]];
+for(list of heroes) {
+    for(heroes of list){
+        console.log(heroes);
+    }
+}
