8136498499385c301710b61a1aea0b722d8e5560
Developer_Chandan <developer.chandan.in@gmail.com>
2026-09-17 19:04:27 +0530

    Add object and task files

---
+++ Object_of_Object.js
@@ -0,0 +1,40 @@
+const classInfo ={
+    chandan:{
+        city: "Delhi",
+        marks: 90
+    },
+
+    Amit: {
+        city: "Goa",
+        marks: 95
+    },
+
+    Aman: {
+        city: "Uttar Pradesh",
+        marks: 70
+    }
+};
+console.log(classInfo);
+
+//Array of Object
+const studentInfo =[
+    {
+        name:"Chandan",
+        city: "Delhi",
+        marks: 90
+    },
+
+    {
+        name: "Amit",
+        city: "Goa",
+        marks: 95
+    },
+
+    {
+        name: "Aman",
+        city: "Uttar Pradesh",
+        marks: 70
+    }
+];
+console.log(studentInfo[1]);
+
---
+++ Task_twitter_post.js
@@ -0,0 +1,9 @@
+const post = {
+    username: "@chandan",
+    content: "this is my #firstPost",
+    likes: 200,
+    reposts: 40,
+    tags: ["@chandan", "#firstPost"]
+};
+
+console.log(post);
